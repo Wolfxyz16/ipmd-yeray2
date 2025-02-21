e14ab71343ffd206a57e05ebe177555d592a1893
Wolfxyz16 <yerayliloaiza@gmail.com>
2025-02-21 11:12:05 +0100

    Finished all the methods

--- trabajo-practico-1/app.py
+++ trabajo-practico-1/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 import mysql.connector
-import json
 
 # mariadb config
 config = {
@@ -23,24 +22,28 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return '{""Message": "Proyecto de ipmd de yeray2"}'
+    return '{"Message": "Hello from {container_id}. Proyecto de ipmd de yeray2"}'
 
 @app.get("/data")
 def get_database():
     """
     El servidor devuelve el contenido completo de la BD, en formato JSON
     """
-    # Connection for MariaDB
-    conn = mysql.connector.connect(**config)
-    # Create a connection cursor
-    cur = conn.cursor()
+    # Hacemos la conexion con la base de datos
+    try:
+        conn = mysql.connector.connect(**config)
+        cur = conn.cursor()
+    except Exception as e:
+        return '{"message": "Can not connect to the database. Maybe wait for a few seconds..."}'
+
     # Execute a SQL statement and fetch de data
     cur.execute("SELECT * FROM messages")
     messages = cur.fetchall()
+
     # Close the connection
     cur.close()
     conn.close()
-    # return the results!
+
     return messages
 
 @app.get("/data/<int:id>")
@@ -48,13 +51,17 @@ def get_id(id):
     """
     El servidor devuelve el registro de la BD identificado por int, en formato JSON
     """
-    # Connection for MariaDB
-    conn = mysql.connector.connect(**config)
-    # Create a connection cursor
-    cur = conn.cursor()
+    # Hacemos la conexion con la base de datos
+    try:
+        conn = mysql.connector.connect(**config)
+        cur = conn.cursor()
+    except Exception as e:
+        return '{"message": "Can not connect to the database. Maybe wait for a few seconds..."}'
+
     # Execute a SQL statement and fetch de data
     cur.execute(f"SELECT * FROM messages WHERE clid={id}")
     message = cur.fetchall()
+
     # Close the connection
     cur.close()
     conn.close()
@@ -85,8 +92,11 @@ def post_message():
         return {"message": "clid is not a valid id. Only integer numbers"}
 
     # Hacemos la conexion con la base de datos
-    conn = mysql.connector.connect(**config)
-    cur = conn.cursor()
+    try:
+        conn = mysql.connector.connect(**config)
+        cur = conn.cursor()
+    except Exception as e:
+        return '{"message": "Can not connect to the database. Maybe wait for a few seconds..."}'
 
     # Ejecutamos la consulta SQL
     try:
@@ -101,7 +111,7 @@ def post_message():
     cur.close()
     conn.close()
 
-    return {"message": "Data added succesfully"}
+    return {"message": "Message added succesfully"}
 
 @app.put("/data/<int:id>")
 def update_message(id):
@@ -109,14 +119,61 @@ def update_message(id):
     El servidor modifica el registro con "clid" = int en la BD. La petición debe incluir datos en JSON con el nuevo contenido del campo "mess".
     El servidor devolverá un mensaje de éxito/error.
     """
-    return f'<p>Updating {id} post...</p>'
+    # Comprobamos que solo haya una key y que sea de nombre mess
+    message = request.get_json()
+    if "mess" not in message.keys() and message.keys() == 1:
+        return {"message": "Message has an invalid format. Must have only mess"}
+
+    new_mess = message['mess']
+
+    # Hacemos la conexion con la base de datos
+    try:
+        conn = mysql.connector.connect(**config)
+        cur = conn.cursor()
+    except Exception as e:
+        return '{"message": "Can not connect to the database. Maybe wait for a few seconds..."}'
+
+    # Ejecutamos la consulta SQL
+    try:
+        cur.execute(f"UPDATE messages SET mess='{new_mess}' WHERE clid='{id}'")
+    except mysql.connector.IntegrityError as e:
+        return {"error": "Integrity error, id not found"}
+
+    # Realizamos los cambios
+    conn.commit()
+
+    # Cerramos la conexion
+    cur.close()
+    conn.close()
+
+    return {"message": "Data updated successfully"}
 
 @app.delete("/data/<int:id>")
 def delete_message(id):
     """
     El servidor elimina el registro con "clid" = int en la BD. El servidor devolverá un mensaje de éxito/error.
     """
-    return f'<p>Deleting {id} post...</p>'
+    # Hacemos la conexion con la base de datos
+    try:
+        conn = mysql.connector.connect(**config)
+        cur = conn.cursor()
+    except Exception as e:
+        return '{"message": "Can not connect to the database. Maybe wait for a few seconds..."}'
+
+    # Ejecutamos la consulta SQL
+    try:
+        cur.execute(f"DELETE FROM messages WHERE clid={id}")
+    except mysql.connector.IntegrityError as e:
+        return {"error": "Integrity error, id not found"}
+
+    # Realizamos los cambios
+    conn.commit()
+
+    # Cerramos la conexion
+    cur.close()
+    conn.close()
+
+    return {"message": "Data deleted successfully"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
