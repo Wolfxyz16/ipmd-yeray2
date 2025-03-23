3a60cbf89e43166f4766fe80534cb55fd2ed139e
ycarretero <ycarretero001@ikasle.ehu.eus>
2025-03-23 12:22:05 +0100

    no se ejecuta lo de CMD ns porque

--- trabajo-practico-2/init_hdfs.sh
+++ trabajo-practico-2/init_hdfs.sh
@@ -3,9 +3,14 @@
 # Crear y asignar permisos en HDFS
 hdfs dfs -mkdir -p /user/hive
 hdfs dfs -chown hive /user/hive
+hdfs dfs -mkdir -p /user/hive/warehouse
+hdfs dfs -chown hive /user/hive/warehouse
+hdfs dfs -mkdir -p /home/hive
+hdfs dfs -chown hive /home/hive
 
 # Crear directorio en HDFS
 hdfs dfs -mkdir -p /user/hive/userdata/
+hdfs dfs -chown hive /user/hive/userdata/
 
 # Subir archivos AVRO a HDFS
 hdfs dfs -put /trabajo-practico-2/userdata/* /user/hive/userdata/
