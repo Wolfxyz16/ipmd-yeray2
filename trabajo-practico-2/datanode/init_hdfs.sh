7a9529fdeccd97eaba8d2ac7184c6370c5615b15
ycarretero <ycarretero001@ikasle.ehu.eus>
2025-03-25 18:55:30 +0100

    da fallos

--- trabajo-practico-2/datanode/init_hdfs.sh
+++ trabajo-practico-2/datanode/init_hdfs.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 # Crear y asignar permisos en HDFS
-hdfs dfs -mkdir -p /user/hive
-hdfs dfs -chown hive /user/hive
-
-hdfs dfs -mkdir -p /user/hive/warehouse
-hdfs dfs -chown hive /user/hive/warehouse
-
-hdfs dfs -mkdir -p /home/hive
-hdfs dfs -chown hive /home/hive
+hadoop fs -mkdir -p hdfs://localhost/user/hive
+hadoop fs -chown hive hdfs://localhost/user/hive
+hadoop fs -mkdir -p hdfs://localhost/user/hive/warehouse
+hadoop fs -chown hive hdfs://localhost/user/hive/warehouse
+hadoop fs -mkdir -p hdfs://localhost/home/hive
+hadoop fs -chown hive hdfs://localhost/home/hive
 
 # Crear directorio en HDFS
-hdfs dfs -mkdir -p /user/hive/userdata/
-hdfs dfs -chown hive /user/hive/userdata/
+hadoop fs -mkdir -p hdfs://localhost/user/hive/userdata/
+hadoop fs -chown hive hdfs://localhost/user/hive/userdata/
 
 # Subir archivos AVRO a HDFS
-hdfs dfs -put /trabajo-practico-2/userdata/* /user/hive/userdata/
+hadoop fs -put /trabajo-practico-2/userdata/* hdfs://localhost/user/hive/userdata/
 
 echo "✅ Datos AVRO cargados en HDFS correctamente."
