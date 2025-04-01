e523febd04e35386ab8568e155de9ceeccd82fcc
Wolfxyz16 <yerayliloaiza@gmail.com>
2025-04-01 20:14:14 +0200

    created grafana user on mariadb

--- trabajo-practico-2/db/init.sql
+++ trabajo-practico-2/db/init.sql
@@ -1,12 +1,13 @@
 -- Creamos los usuarios
 CREATE USER IF NOT EXISTS 'wolfxyz'@'%' IDENTIFIED BY 'wolfxyz';
+CREATE USER IF NOT EXISTS 'grafana'@'%' IDENTIFIED BY 'grafana';
 
 -- Creamos la base de datos
 CREATE DATABASE IF NOT EXISTS ipmd;
 
 -- Damos privilegios a los usuarios
--- prometheus necesita todos los permisos para monitorizar
 GRANT ALL PRIVILEGES ON ipmd TO 'wolfxyz'@'%';
+GRANT SELECT ON ipmd.summary TO 'grafana'@'%';
 GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'wolfxyz'@'%';
 GRANT SLAVE MONITOR ON *.* TO 'wolfxyz'@'%';
 GRANT REPLICATION CLIENT ON *.* TO 'wolfxyz'@'%';
